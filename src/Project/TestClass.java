4892d9e86b365fd546c0d451f698111b899699e8
ahmetcetinkaya1 <73968684+ahmetcetinkaya1@users.noreply.github.com>
2020-11-06 22:21:47 +0100

    Update TestClass.java

--- src/Project/TestClass.java
+++ src/Project/TestClass.java
@@ -3,7 +3,8 @@ package Project;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("merhabalar");
+		System.out.println("GITHUBdan merhabalar");
 
 	}
 
